c5ca0914933edc66be8818e5928d36e703f9e1b8
AVVS <v@makeomatic.ru>
2016-11-18 22:52:12 +0300

    feat: adds direct http webhook support

--- src/actions/instagram/webhook.js
+++ src/actions/instagram/webhook.js
@@ -10,32 +10,60 @@ const { SERVICE_FEED, SERVICE_INSTAGRAM, NETWORK_INSTAGRAM } = require('../..');
  * @apiGroup Instagram
  * @apiSchema {jsonschema=../../../schemas/instagram.webhook.json} apiParam
  */
-function webhookAction({ params }) {
-  const feedService = this.getService(SERVICE_FEED);
-  const instagramService = this.getService(SERVICE_INSTAGRAM);
-
-  if (params['hub.mode'] === 'subscribe') {
-    return instagramService.verifySubcription(params);
+function webhookAction({ params, query, method }) {
+  if (method === 'get') {
+    return webhookAction.verify.call(this, query);
   }
 
-  return Promise
-    .map(values(params), (subscription) => {
-      const { object_id: networkId, data: { media_id: mediaId } } = subscription;
+  if (method === 'post') {
+    return webhookAction.hook.call(this, params);
+  }
 
-      return feedService
-        .getByNetworkId(NETWORK_INSTAGRAM, networkId)
-        .tap((feed) => {
-          if (feed === undefined) {
-            throw new Errors.NotFoundError(`Feed for user #${networkId}`);
-          }
-        })
-        .then(feed => instagramService.fetchMedia(mediaId, feed.meta.token));
-    })
-    .map(media => instagramService.saveMedia(media))
-    .then(({ length }) => ({ media: length }));
+  if (method === 'amqp' && params['hub.mode'] === 'subscribe') {
+    return webhookAction.verify.call(this, params);
+  }
+
+  return webhookAction.hook.call(this, params);
 }
 
+/**
+ * Accepts webhook verification request and completes the challenge
+ * @param  {Object} query
+ * @return {Promise}
+ */
+webhookAction.verify = function verify(query) {
+  const instagramService = this.getService(SERVICE_INSTAGRAM);
+  return instagramService.verifySubcription(query);
+};
+
+/**
+ * Accepts webhook data and processes it
+ * @param  {Object} params
+ * @return {PRomise}
+ */
+webhookAction.hook = function hook(params) {
+  const feedService = this.getService(SERVICE_FEED);
+  const instagramService = this.getService(SERVICE_INSTAGRAM);
+  const rawMediaData = values(params);
+
+  return Promise.map(rawMediaData, (subscription) => {
+    const { object_id: networkId, data: { media_id: mediaId } } = subscription;
+
+    return feedService
+      .getByNetworkId(NETWORK_INSTAGRAM, networkId)
+      .tap((feed) => {
+        if (feed) return;
+
+        throw new Errors.NotFoundError(`Feed for user #${networkId}`);
+      })
+      .then(feed => instagramService.fetchMedia(mediaId, feed.meta.token));
+  })
+  .map(media => instagramService.saveMedia(media))
+  .then(({ length }) => ({ media: length }));
+};
+
+// internals
 webhookAction.schema = 'instagram.webhook';
-webhookAction.transports = ['amqp'];
+webhookAction.transports = ['amqp', 'http'];
 
 module.exports = webhookAction;
