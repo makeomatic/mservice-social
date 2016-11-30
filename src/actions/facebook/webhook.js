46706efc6db7b4c913b1fa60249335cf14116139
Aleksandr Sorokin <imarkoffko@gmail.com>
2016-11-30 17:06:06 +0300

    fix: feed register documentation (#13)

--- src/actions/facebook/webhook.js
+++ src/actions/facebook/webhook.js
@@ -4,7 +4,7 @@ const { NotSupportedError } = require('common-errors');
  * @api {http} <prefix>.facebook.webhook Verify subscription, save media from facebook
  * @apiVersion 1.0.0
  * @apiName facebook.webhook
- * @apiGroup facebook
+ * @apiGroup Facebook
  * @apiSchema {jsonschema=../../../schemas/facebook.webhook.json} apiParam
  */
 function webhookAction({ params, query, method }) {
