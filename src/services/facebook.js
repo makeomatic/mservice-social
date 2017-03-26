d29b66e2ce1c5380ea60fb5445f243818d6d25af
AVVS <v@makeomatic.ru>
2017-03-26 11:15:22 -0700

    feat: update retry handling

--- src/services/facebook.js
+++ src/services/facebook.js
@@ -3,9 +3,18 @@ const omit = require('lodash/omit');
 const proofGenerator = require('./facebook/proof-generator');
 const Promise = require('bluebird');
 const request = require('request-promise');
+const retry = require('retry');
 const Subscription = require('./facebook/subscription');
 
 class Facebook {
+  static timeoutOptions = {
+    retries: Infinity,
+    factor: 2,
+    minTimeout: 60000,
+    maxTimeout: Infinity,
+    randomize: true,
+  };
+
   constructor(config, storage, feed, logger) {
     this.config = config;
     this.feed = feed;
@@ -16,17 +25,40 @@ class Facebook {
     this.subscription = new Subscription(this);
   }
 
-  request(options, accessToken) {
+  handleError(response) {
+    const error = response.error && response.error.error;
+
+    /**
+     * 4: Application-level throttling 200 calls/person/hour
+     * 32: Page-level throttling 4800 calls/person/24-hours
+     */
+    if (error && [4, 32].includes(error.code)) {
+      const timeout = retry.createTimeout(this.attempt, Facebook.timeoutOptions);
+
+      // notify of throttling error
+      this.logger.warn('Trying to repeat request after %d ms because', timeout, error);
+
+      return Promise
+        .bind(this.ctx, [this.options, this.accessToken, this.attempt + 1])
+        .delay(timeout)
+        .spread(this.request);
+    }
+
+    return Promise.reject(response);
+  }
+
+  request(options, accessToken, attempt = 1) {
     const method = options.method || 'get';
     const path = options.url;
     const { version } = this.config.api;
+
     const requestOptions = Object.assign(
       {
         json: true,
         qs: {},
         url: `https://graph.facebook.com/${version}${path}`,
       },
-      omit(options, 'method', 'url')
+      omit(options, 'method', 'url', 'retry', 'attempt')
     );
     const handler = request[method];
 
@@ -37,21 +69,8 @@ class Facebook {
 
     return Promise
       .bind(this, handler(requestOptions))
-      .catch((response) => {
-        const error = response.error && response.error.error;
-
-        /**
-         * 4: Application-level throttling 200 calls/person/hour
-         * 32: Page-level throttling 4800 calls/person/24-hours
-         */
-        if (error && [4, 32].includes(error.code)) {
-          this.logger.error('Trying to repeat request after 1000*60ms because', error);
-
-          return Promise.bind(this, options).delay(1000 * 60).then(this.request);
-        }
-
-        throw response;
-      });
+      .bind({ options, accessToken, attempt, ctx: this })
+      .catch(this.handleError);
   }
 }
 
