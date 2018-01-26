72650272d2784df7799490b9059063577ab4e311
AVVS <v@makeomatic.ru>
2018-01-25 21:45:06 -0800

    fix: message check

--- src/services/twitter.js
+++ src/services/twitter.js
@@ -167,7 +167,7 @@ class Twitter {
     if (Array.isArray(exception) && exception.find(it => (it.code === 34))) {
       // do not reconnect, but try to identify account that has been deleted
       this.logger.warn('account erased from', exception);
-    } else if (exception === 'Status Code: 420') {
+    } else if (exception.message === 'Status Code: 420') {
       this.destroy();
       this.logger.warn('stream connection rate limit, reconnect in 10s', exception.message);
       this.reconnect = Promise.bind(this).delay(10000).then(this.init);
