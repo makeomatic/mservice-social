a6b5a8e04ae939c2c0300eca3214ffda958f01df
Eugene Poyarkov <eug.poyarkov@gmail.com>
2018-12-15 09:51:20 +0300

    fix: lint

--- src/migrations/20181214194528_tweets-usernames-index.js
+++ src/migrations/20181214194528_tweets-usernames-index.js
@@ -1,7 +1,7 @@
-exports.up = function(knex) {
+exports.up = function up(knex) {
   return knex.schema.raw('create index idx_tweets_account on statuses using BTREE ((meta->>\'account\'))');
 };
 
-exports.down = function(knex) {
+exports.down = function down(knex) {
   return knex.schema.raw('drop index idx_tweets_account');
 };
