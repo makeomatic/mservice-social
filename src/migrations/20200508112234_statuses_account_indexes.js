9e629ab13b76cbecf208d376ceedd522f69ad4e3
Renat Berezovskiy <renat0014@gmail.com>
2020-05-12 17:49:47 +0400

    fix: make indexes in background

--- src/migrations/20200508112234_statuses_account_indexes.js
+++ src/migrations/20200508112234_statuses_account_indexes.js
@@ -1,12 +1,12 @@
 exports.up = async (knex) => {
   return knex.schema.raw(`
-    CREATE INDEX IF NOT EXISTS idx_tweets_id_account on statuses using BTREE (id DESC, account);
-    CREATE INDEX IF NOT EXISTS idx_tweets_account_id on statuses using BTREE (account, id DESC);
-    DROP INDEX idx_tweets_ids_lower_account;
-    DROP INDEX idx_tweets_lower_account;
-    DROP INDEX idx_tweets_lower_account_ids;
-    DROP INDEX idx_tweets_account;
-    CREATE INDEX IF NOT EXISTS idx_tweets_account on statuses using BTREE (account);
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tweets_id_account on statuses using BTREE (id DESC, account);
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tweets_account_id on statuses using BTREE (account, id DESC);
+    DROP INDEX IF EXISTS idx_tweets_ids_lower_account;
+    DROP INDEX IF EXISTS idx_tweets_lower_account;
+    DROP INDEX IF EXISTS idx_tweets_lower_account_ids;
+    DROP INDEX IF EXISTS idx_tweets_account;
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tweets_account on statuses using BTREE (account);
   `);
 };
 
