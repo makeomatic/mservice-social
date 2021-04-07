00c10805f18ff7ba4a7e734aa38a9d13b119720f
sky3d <sky3ddd@gmail.com>
2021-04-07 22:31:46 +0300

    fix: correct sql query, lint warn

--- src/migrations/20210407170317_statuses_account_index.js
+++ src/migrations/20210407170317_statuses_account_index.js
@@ -3,15 +3,15 @@ const kIndexDescName = 'idx_tweets_id_desc_account';
 const kTable = 'statuses';
 
 exports.up = async (knex) => {
-  return knex.raw(
-    `CREATE INDEX EXISTS ${kIndexAscName} on ${kTable} using BTREE (id ASC, account);` +
-    `CREATE INDEX EXISTS ${kIndexDescName} on ${kTable} using BTREE (id DESC, account)`
-  );
+  return knex.raw(`
+CREATE INDEX IF NOT EXISTS ${kIndexAscName} on ${kTable} using BTREE (id, account);
+CREATE INDEX IF NOT EXISTS ${kIndexDescName} on ${kTable} using BTREE (id DESC, account);
+`);
 };
 
 exports.down = async (knex) => {
   return knex.schema.table(kTable, (table) => {
-    table.dropIndex(null, kIndexDescName)
-    table.dropIndex(null, kIndexAscName)
+    table.dropIndex(null, kIndexDescName);
+    table.dropIndex(null, kIndexAscName);
   });
 };
