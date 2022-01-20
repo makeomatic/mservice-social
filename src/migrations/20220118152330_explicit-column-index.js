914ae6542368cd3b807e346d3e9503d571c7d015
sky3d <sky3ddd@gmail.com>
2022-01-20 11:58:59 +0300

    fix: add statuses indecies for accounts

--- src/migrations/20220118152330_explicit-column-index.js
+++ src/migrations/20220118152330_explicit-column-index.js
@@ -1,13 +1,25 @@
+const kTable = 'statuses';
+
 exports.up = function up(knex) {
-  return knex.schema.alterTable('statuses', (table) => {
+  knex.schema.alterTable('statuses', (table) => {
     table.boolean('explicit').notNullable().defaultTo(false);
-    table.index(['account', 'explicit']);
   });
+
+  knex.schema
+  .raw(`CREATE INDEX IF NOT EXISTS idx_tweets_id_asc_account on ${kTable} using BTREE (id, 'account')`);
+
+  knex.schema
+  .raw(`CREATE INDEX IF NOT EXISTS idx_tweets_id_desc_account on ${kTable} using BTREE (id DESC, 'account')`);
+
+  knex.schema
+  .raw(`CREATE INDEX IF NOT EXISTS idx_account_explicit on ${kTable} using BTREE ('account', 'expilicit' )`);
 };
 
 exports.down = function up(knex) {
   return knex.schema.alterTable('statuses', (table) => {
     table.dropColumn('explicit');
-    table.dropIndex(['account', 'explicit']);
+    table.dropIndex(null, 'idx_account_explicit');
+    table.dropIndex(null, 'idx_tweets_id_desc_account');
+    table.dropIndex(null, 'idx_tweets_id_asc_account');
   });
 };
