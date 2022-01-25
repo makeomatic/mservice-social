6021e54e4ecf01edd60ef29cc4f8cbee97c7c0d7
sky3d <sky3ddd@gmail.com>
2022-01-25 10:06:13 +0300

    fix: create indecies in background

--- src/migrations/20220118152330_explicit-column-index.js
+++ src/migrations/20220118152330_explicit-column-index.js
@@ -7,20 +7,21 @@ exports.up = async (knex) => {
     });
 
   await knex.schema
-    .raw(`CREATE INDEX IF NOT EXISTS idx_tweets_id_asc_account on ${kTable} using BTREE (id, account)`);
+    .raw(`CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tweets_id_asc_account on ${kTable} using BTREE (id, account)`);
 
   await knex.schema
-    .raw(`CREATE INDEX IF NOT EXISTS idx_tweets_id_desc_account on ${kTable} using BTREE (id DESC, account)`);
+    .raw(`CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_tweets_id_desc_account on ${kTable} using BTREE (id DESC, account)`);
 
   await knex.schema
-    .raw(`CREATE INDEX IF NOT EXISTS idx_account_explicit on ${kTable} using BTREE (account, explicit)`);
+    .raw(`CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_account_explicit on ${kTable} using BTREE (account, explicit)`);
 };
 
 exports.down = async (knex) => {
-  return knex.schema.alterTable('statuses', (table) => {
-    table.dropColumn('explicit');
-    table.dropIndex(null, 'idx_account_explicit');
-    table.dropIndex(null, 'idx_tweets_id_desc_account');
-    table.dropIndex(null, 'idx_tweets_id_asc_account');
-  });
+  return knex.schema
+    .alterTable('statuses', (table) => {
+      table.dropColumn('explicit');
+      table.dropIndex(null, 'idx_account_explicit');
+      table.dropIndex(null, 'idx_tweets_id_desc_account');
+      table.dropIndex(null, 'idx_tweets_id_asc_account');
+    });
 };
