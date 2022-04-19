547989c25a9b23e9582bf32f2e7bed597a698a3f
sky3d <sky3ddd@gmail.com>
2022-04-19 12:42:58 +0300

    fix: update delete handler

--- src/migrations/20220418091527_statuses_deleted_at.js
+++ src/migrations/20220418091527_statuses_deleted_at.js
@@ -1,10 +1,6 @@
 const kIndexName = 'idx_tweets_account_deleted_at';
 const kTable = 'statuses';
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = async (knex) => {
   await knex.schema.alterTable(kTable, (table) => {
     table.timestamp('deleted_at');
@@ -12,13 +8,8 @@ exports.up = async (knex) => {
 
   await knex.schema
     .raw(`CREATE INDEX IF NOT EXISTS ${kIndexName} on ${kTable} using BTREE (account, deleted_at)`);
-
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = async (knex) => {
   return knex.schema.alterTable(kTable, (table) => {
     table.dropColumn('deleted_at');
