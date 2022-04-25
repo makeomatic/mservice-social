5b860d301805e9ac9f4a499cd78439b321fc33fe
sky3d <sky3ddd@gmail.com>
2022-04-25 22:22:57 +0300

    fix: add partial index for id

--- src/migrations/20220418091527_statuses_deleted_at.js
+++ src/migrations/20220418091527_statuses_deleted_at.js
@@ -1,4 +1,5 @@
 const kIndexName = 'idx_tweets_account_deleted_at';
+const kPartialIndexName = 'idx_id_partial_deleted_at';
 const kTable = 'statuses';
 
 exports.up = async (knex) => {
@@ -8,10 +9,15 @@ exports.up = async (knex) => {
 
   await knex.schema
     .raw(`CREATE INDEX IF NOT EXISTS ${kIndexName} on ${kTable} using BTREE (account, deleted_at)`);
+
+  await knex.schema
+  .raw(`CREATE INDEX IF NOT EXISTS ${kIndexName} on ${kTable} (id) WHERE deleted_at IS NULL`);
+
 };
 
 exports.down = async (knex) => {
   return knex.schema.alterTable(kTable, (table) => {
+    table.dropIndex(null, kPartialIndexName);
     table.dropIndex(null, kIndexName);
     table.dropColumn('deleted_at');
   });
