afee63d9150ebb0c263bbf7a6df5ab9b90761474
sky3d <sky3ddd@gmail.com>
2022-04-27 11:01:07 +0300

    fix: get rid partial indexes from migration

--- src/migrations/20220418091527_statuses_deleted_at.js
+++ src/migrations/20220418091527_statuses_deleted_at.js
@@ -1,23 +1,13 @@
-const kPartialIdIndexName = 'idx_id_partial_deleted_at';
-const kPartialAccountIndexName = 'idx_account_partial_deleted_at';
 const kTable = 'statuses';
 
 exports.up = async (knex) => {
   await knex.schema.alterTable(kTable, (table) => {
     table.timestamp('deleted_at');
   });
-
-  await knex.schema
-    .raw(`CREATE INDEX IF NOT EXISTS ${kPartialIdIndexName} on ${kTable} (id) WHERE deleted_at IS NULL`);
-
-  await knex.schema
-    .raw(`CREATE INDEX IF NOT EXISTS ${kPartialAccountIndexName} on ${kTable} (account) WHERE deleted_at IS NULL`);
 };
 
 exports.down = async (knex) => {
   return knex.schema.alterTable(kTable, (table) => {
-    table.dropIndex(null, kPartialAccountIndexName);
-    table.dropIndex(null, kPartialIdIndexName);
     table.dropColumn('deleted_at');
   });
 };
