2f4652820c41251d5525826f14e393445c563c1d
sky3d <sky3ddd@gmail.com>
2022-11-22 13:56:05 +0300

    fix: add twitter requests config, types enum

--- src/utils/twitter.js
+++ src/utils/twitter.js
@@ -1,6 +1,13 @@
 const isNil = require('lodash/isNil');
 const get = require('get-value');
 
+const TweetTypes = {
+  REPLY: 'reply',
+  RETWEET: 'retweet',
+  USER_MENTIONS: 'userMentions',
+  HASHTAGS: 'hashTags',
+};
+
 function isRetweet(data) {
   return !isNil(data.retweeted_status);
 }
@@ -25,20 +32,21 @@ function hasHashTags(data) {
 function collectTweetTypes(data) {
   const types = {};
   if (isReply(data)) {
-    types.reply = 1;
+    types[TweetTypes.REPLY] = 1;
   }
   if (isRetweet(data)) {
-    types.retweet = 1;
+    types[TweetTypes.RETWEET] = 1;
   }
   if (hasUserMentions(data)) {
-    types.userMentions = 1;
+    types[TweetTypes.USER_MENTIONS] = 1;
   }
   if (hasHashTags(data)) {
-    types.hashTags = 1;
+    types[TweetTypes.HASHTAGS] = 1;
   }
   return types;
 }
 
 module.exports = {
+  TweetTypes,
   collectTweetTypes,
 };
