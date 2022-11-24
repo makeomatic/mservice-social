324ce39e698f69e6e0873b6b941d514089ef8550
sky3d <sky3ddd@gmail.com>
2022-11-24 13:40:41 +0300

    fix: add indexes for statuses: id, account and type

--- src/migrations/20221121133715_tweet-type-column.js
+++ src/migrations/20221121133715_tweet-type-column.js
@@ -4,10 +4,18 @@ exports.up = async (knex) => {
   await knex.schema.alterTable(kTable, (table) => {
     table.bigInteger('type');
   });
+
+  await knex.schema
+    .raw(`CREATE INDEX IF NOT EXISTS idx_tweets_id_asc_account_type on ${kTable} using BTREE (id, account, type)`);
+  await knex.schema
+    .raw(`CREATE INDEX IF NOT EXISTS idx_tweets_id_desc_account_type on ${kTable} using BTREE (id DESC, account, type)`);
 };
 
 exports.down = async (knex) => {
+
   return knex.schema.alterTable(kTable, (table) => {
+    table.dropIndex(null, 'idx_tweets_id_desc_account_type');
+    table.dropIndex(null, 'idx_tweets_id_asc_account_type');
     table.dropColumn('type');
   });
 };
