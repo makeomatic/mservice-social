5cfa9d6fb84608f744f420466fab0707ba5b311a
sky3d <sky3ddd@gmail.com>
2022-11-28 23:17:22 +0300

    refactor: cleanup code, remove comments

--- test/suites/03.notifier.js
+++ test/suites/03.notifier.js
@@ -1,10 +1,9 @@
 const Promise = require('bluebird');
-const is = require('is');
 const sinon = require('sinon');
 const assert = require('assert');
 
 describe('notifier', function testSuite() {
-  // this.retries(20);
+  this.retries(20);
 
   const Social = require('../../src');
   const Notifier = require('../../src/services/notifier');
@@ -13,44 +12,15 @@ describe('notifier', function testSuite() {
     const config = {
       ...global.SERVICES,
       notifier: {
-        enabled: true, // false TODO: add tests for disabled
+        enabled: true,
       },
     };
     const service = this.service = new Social(config);
     return service.connect();
   });
 
-  // it('returns null when there is no link to core service', () => {
-  //   assert(Notifier.getInstance() === null);
-  // });
-
-  it.skip('returns plugin connector', () => {
-    const { connect, close } = this.connector = Notifier.connector(this.service);
-    assert(is.fn(connect));
-    assert(is.fn(close));
-  });
-
-  it.skip('both connector and destructor return false when notifier not initialized', () => {
-    const { connect, close } = this.connector;
-    // const stubbedGetInstance = sinon.stub(Notifier, 'getInstance')
-    //   .callsFake(() => null);
-
-    assert(connect() === false);
-    assert(close() === false);
-
-    // stubbedGetInstance.restore();
-  });
-
-  // it('throws an error when trying to initialize connector again', () => {
-  //   assert.throws(() => Notifier.connector(), Notifier.kDupInstance);
-  // });
-
-  // it('returns null when skipInitialization is set to true', () => {
-  //   assert(Notifier.getInstance(true) === null);
-  // });
-
   it('returns an instance of notifier', () => {
-    const notifier = this.notifier = this.service.service(Notifier.SERVICE_NOTIFIER); // Notifier.getInstance();
+    const notifier = this.notifier = this.service.service(Notifier.SERVICE_NOTIFIER);
     assert(notifier instanceof Notifier);
     assert(notifier.core instanceof Social);
     assert(notifier.namespace);
@@ -58,18 +28,8 @@ describe('notifier', function testSuite() {
     assert(notifier.amqpConfig);
   });
 
-  // it('returns the same instance of notifier on each call', () => {
-  //   assert(Notifier.getInstance() === this.notifier);
-  // });
-
-  it.skip('returns null when tried to call .publish when disconnected', async () => {
-    assert(await this.notifier.publish() === null);
-  });
-
   it('aqmp client is ready', async () => {
-    // it('is able to connect', async () => {
     await Promise.delay(1500);
-    // await this.connector.connect();
     assert(this.notifier.amqpClient);
     assert(this.service.listenerCount(Notifier.kPublishEvent) === 1);
   });
@@ -103,18 +63,12 @@ describe('notifier', function testSuite() {
   });
 
   after('close', async () => {
-    // this.amqpClientStub.restore();
     try {
-      // make another attempt to close a connection if one of the tests has failed
-
-      // await this.connector.close();
       if (this.service !== null) {
         await this.service.close();
       }
     } catch (e) {
       // nothing
-    } finally {
-      // await this.service.close();
     }
   });
 });
