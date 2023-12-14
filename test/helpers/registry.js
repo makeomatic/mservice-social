7b4a755c01f770f7b72e6fd0744f10615d47e6b3
kristobalus <ildar.tamendarov@gmail.com>
2023-12-14 12:00:38 +0600

    fix: tests

--- test/helpers/registry.js
+++ test/helpers/registry.js
@@ -1,2 +1,4 @@
+// eslint-disable-next-line func-names
 global.FinalizationRegistry = function () {};
+// eslint-disable-next-line func-names
 global.FinalizationRegistry.prototype.register = function () {};
